d27063acb09b134fbfbd64b280b3b07748712fdc
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 20:11:56 +0200

    solve ex2.18

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 function App() {
+  let [countries, setCountries] = useState(null)
+  let [filter, setFilter] = useState('')
+
+  let countriesToShow = countries?.filter((country) =>
+    country.name.common.toLowerCase().includes(filter.toLowerCase()),
+  )
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then((response) => setCountries(response.data))
+  }, [])
+
   return (
     <>
       <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
@@ -9,11 +25,54 @@ function App() {
           type="search"
           name="fitler"
           id="fitler"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          disabled={!countries}
           placeholder="e.g. Slovakia"
         />
       </div>
       <div style={{ marginTop: 16 }}>
-        Too many matches, specify another filter
+        {countriesToShow ? (
+          countriesToShow.length > 10 ? (
+            'Too many matches, specify another filter'
+          ) : countriesToShow.length > 1 ? (
+            <ul>
+              {countriesToShow.map((country) => (
+                <li key={country.name.common}>{country.name.common}</li>
+              ))}
+            </ul>
+          ) : (
+            <div>
+              <h2>{countriesToShow[0].name.common}</h2>
+              <dl>
+                <dt>Capital</dt>
+                <dd>{countriesToShow[0].capital}</dd>
+                <dt>
+                  Area (km<sup>2</sup>)
+                </dt>
+                <dd>
+                  {countriesToShow[0].area.toLocaleString('en-US', {
+                    style: 'decimal',
+                  })}
+                </dd>
+              </dl>
+              <h3>Languages</h3>
+              <ul>
+                {Object.values(countriesToShow[0].languages).map((language) => (
+                  <li key={language}>{language}</li>
+                ))}
+              </ul>
+              <img
+                src={countriesToShow[0].flags.svg}
+                alt={countriesToShow[0].flags.alt}
+                width={120}
+                height={80}
+              />
+            </div>
+          )
+        ) : (
+          'Loading…'
+        )}
       </div>
     </>
   )
